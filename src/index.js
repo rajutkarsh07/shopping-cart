1c6b95f476d9e5004998474d351a9dd28ed9c8fc
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-09-30 17:49:37 +0545

    added a error handling for the context

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ import Context from "./context/Context";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Context>
-      <App />
+     <App />
+     <Context>
+    
+   
+   
+      
     </Context>
   </React.StrictMode>
 );
